1c01917baf10bca9e765ea7d10155408a69d1168
Hugh Sharp <hieubui.fsb@gmail.com>
2026-04-02 03:59:40 +0700

    Updates

--- projectScripts/test/point-test.js
+++
@@ -1,8 +0,0 @@
-export function initPointTest(scene) {
-    const moveBtn = document.getElementById('move-btn');
-    if (moveBtn) {
-        moveBtn.addEventListener('click', () => {
-            console.log('clicked');
-        });
-    }
-}
